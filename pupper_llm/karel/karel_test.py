8333c1570ebefbf9d81eb7f89451107b34bae592
stong88 <70980554+stong88@users.noreply.github.com>
2025-12-06 17:03:18 -0800

    add move_coordinate to karel_test

--- pupper_llm/karel/karel_test.py
+++ pupper_llm/karel/karel_test.py
@@ -5,12 +5,15 @@ import karel
 
 def main():
     pupper = karel.KarelPupper()
-    pupper.move_forward()
-    pupper.wiggle()
-    pupper.move_backward()
-    pupper.wiggle()
-    pupper.turn_right()
-    pupper.turn_left()
+    pupper.move_coordinate((-1, 0))  # left
+    pupper.move_coordinate((1, 0))  # right
+    pupper.move_coordinate((1, 1))  # diagonal to right
+    # pupper.move_forward()
+    # pupper.wiggle()
+    # pupper.move_backward()
+    # pupper.wiggle()
+    # pupper.turn_right()
+    # pupper.turn_left()
     
     # pupper.wiggle()
     # pupper.bob()
